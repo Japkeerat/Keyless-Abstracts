3b1ec43194b32c6d5ebdf00a06857599c1571aa7
Japkeerat <japkeerat21@gmail.com>
2020-07-02 09:19:16 +0530

    Added support for extracting data for particular year of papers.

--- DataCuration/main.py
+++ DataCuration/main.py
@@ -17,9 +17,16 @@ def main(config):
     url_list = extract_subjects(config['Arxiv_Website'], config['Subjects'])
     logging.info("Number of subjects found is {}".format(len(url_list)))
     if len(url_list) != 0:
-        for url in tqdm(url_list, desc="Subjects"):
-            extract_content_year_wise(config['Arxiv_Website'], url)
-            logging.info("Done with {}".format(url))
+        if 'all' not in config['Subjects']:
+            idx = 0
+            keys = list(config['Subjects'].keys())
+            for url in tqdm(url_list, desc="Subjects"):
+                extract_content_year_wise(config['Arxiv_Website'], url, config['Subjects'][keys[idx]])
+                logging.info("Done with {}".format(url))
+        else:
+            for url in tqdm(url_list, desc="Subjects"):
+                extract_content_year_wise(config['Arxiv_Website'], url, None)
+                logging.info("Done with {}".format(url))
     logging.info("Done")
 
 
